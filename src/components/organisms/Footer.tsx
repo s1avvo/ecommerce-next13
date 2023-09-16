acc128b2f9b23448ab4300febf791e7e328cc710
s1avo <s.nadolny@hotmail.com>
2023-09-16 14:04:45 +0200

    feat(footer): link to Policies and rules

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+import { DocumentTextIcon } from "@heroicons/react/24/outline";
+
 export const Footer = () => {
 	return (
-		<footer className="bg-wh-900 text-wh-10 flex w-full items-center justify-center px-36 py-4">
-			&copy;2023
+		<footer className="bg-wh-900 text-wh-10 flex w-full items-center justify-between bg-neutral-900 px-6 py-4 text-neutral-100 sm:px-36">
+			<p>ECOMMERCE &copy;2023</p>
+			<Link href={"/policies"} className="flex gap-2">
+				<DocumentTextIcon className="h-5 w-5" />
+				<p>Policies and rules.</p>
+			</Link>
 		</footer>
 	);
 };
