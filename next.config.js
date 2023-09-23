dbec955509a7bcea8d92c23a6e64d1b13a689d07
s1avo <s.nadolny@hotmail.com>
2023-09-23 20:27:27 +0200

    ref(): add redirect paths

--- next.config.js
+++ next.config.js
@@ -15,13 +15,18 @@ const nextConfig = {
 				permanent: false,
 			},
 			{
-				source: "/products/t-shirts",
-				destination: "/products/t-shirts/1",
+				source: "/categories/t-shirts",
+				destination: "/categories/t-shirts/1",
 				permanent: false,
 			},
 			{
-				source: "/products/hoodies",
-				destination: "/products/hoodies/1",
+				source: "/categories/hoodies",
+				destination: "/categories/hoodies/1",
+				permanent: false,
+			},
+			{
+				source: "/categories/accessories",
+				destination: "/categories/accessories/1",
 				permanent: false,
 			},
 		];
