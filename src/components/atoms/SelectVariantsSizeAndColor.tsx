a4437874ed59c578b48b24ff55d16068c37d4b44
s1avo <s.nadolny@hotmail.com>
2023-09-23 20:20:20 +0200

    feat(product): product variants

---
+++ src/components/atoms/SelectVariantsSizeAndColor.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { type Route } from "next";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+import { type SingleProductSizeColorVariantFragment } from "@/gql/graphql";
+
+type SelectVariantsSizeAndColorProps = {
+	sizeAndColor?: SingleProductSizeColorVariantFragment[];
+};
+export const SelectVariantsSizeAndColor = ({ sizeAndColor }: SelectVariantsSizeAndColorProps) => {
+	const router = useRouter();
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+
+	const createQueryString = useCallback(
+		(name: string, value: string) => {
+			const params = new URLSearchParams(searchParams);
+			params.set(name, value);
+
+			return params.toString();
+		},
+		[searchParams],
+	);
+
+	return (
+		<>
+			{sizeAndColor && sizeAndColor[0] && (
+				<select
+					name="variants-size"
+					id="variants-size-id"
+					value={searchParams.get("sizeAndColor") || "Size/Color"}
+					onChange={(event) =>
+						router.push(
+							`${pathname}?${createQueryString("sizeAndColor", event.target.value)}` as Route,
+						)
+					}
+				>
+					<option disabled selected>
+						Size/Color
+					</option>
+					{sizeAndColor.map((v) => (
+						<option key={v.id} value={v.name}>
+							{v.size}/{v.color}
+						</option>
+					))}
+				</select>
+			)}
+		</>
+	);
+};
