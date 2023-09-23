ed1856f88314de60f25abb68265ab8897f404a3a
s1avo <s.nadolny@hotmail.com>
2023-09-23 20:18:02 +0200

    feat(product): suggested products list

--- src/components/organisms/SuggestedProductList.tsx
+++ src/components/organisms/SuggestedProductList.tsx
@@ -1,11 +1,13 @@
-import { getProductsList } from "@/api/getProductsList";
+import { type ProductListItemFragment } from "@/gql/graphql";
 import { ProductList } from "@/components/organisms/ProductList";
 
 /*const sleep = async (ms: number) => {
 	await new Promise((resolve) => setTimeout(resolve, ms));
 };*/
-export const SuggestedProductList = async () => {
-	const products = await getProductsList();
+type SuggestedProductListProps = {
+	products: ProductListItemFragment[];
+};
+export const SuggestedProductList = async ({ products }: SuggestedProductListProps) => {
 	// await sleep(5000);
-	return <ProductList products={products.slice(0, 4)} />;
+	return <ProductList products={products} />;
 };
