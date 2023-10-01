fa81bed2d9f3bca31e11984934132b8aff3ee1c8
s1avo <s.nadolny@hotmail.com>
2023-10-01 19:48:46 +0200

    feat(cart): cart service

---
+++ src/components/molecules/AddToCart.tsx
@@ -0,0 +1,34 @@
+import { type SingleProductItemFragment } from "@/gql/graphql";
+import { addOrUpdateProductToCart, getOrCreateCart } from "@/api/cart";
+import { SubmitButton } from "@/components/atoms/SubmitButton";
+
+type AddToCartProps = {
+	product: SingleProductItemFragment;
+};
+
+export const AddToCart = ({ product }: AddToCartProps) => {
+	async function addProductToCartAction() {
+		"use server";
+
+		const cart = await getOrCreateCart();
+		const orderItem = cart.orderItems.find((item) =>
+			item.product?.id === product.id ? item : undefined,
+		);
+
+		await addOrUpdateProductToCart(
+			cart.id,
+			product.id,
+			orderItem ? orderItem.id : undefined,
+			orderItem ? orderItem.quantity + 1 : 1,
+			orderItem ? product.price * (orderItem.quantity + 1) : product.price,
+		);
+	}
+
+	return (
+		<div className="mt-10 flex items-center">
+			<form action={addProductToCartAction}>
+				<SubmitButton label={"ADD TO CART"} />
+			</form>
+		</div>
+	);
+};
