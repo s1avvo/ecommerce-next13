c00ca379d1a9bf17414180a355933c05f2138fac
s1avo <s.nadolny@hotmail.com>
2023-10-01 20:04:44 +0200

    feat(product): add loading to product

---
+++ src/app/product/[productId]/loading.tsx
@@ -0,0 +1,9 @@
+import { Loading } from "@/components/atoms/Loading";
+
+export default function LoadingProduct() {
+	return (
+		<div className="flex min-h-screen items-center justify-center">
+			<Loading />;
+		</div>
+	);
+}
