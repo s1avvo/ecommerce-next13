15acaa29dd6158d5205911e6ee250d745a1af992
s1avo <s.nadolny@hotmail.com>
2023-10-01 19:57:15 +0200

    ref(): add data-testid

--- src/components/atoms/ProductListItemDescription.tsx
+++ src/components/atoms/ProductListItemDescription.tsx
@@ -15,7 +15,9 @@ export const ProductListItemDescription = ({
 				<h3 className="text-sm text-gray-900">{name}</h3>
 				{categories[0] && <p className="text-sm text-gray-500">{categories[0]?.name}</p>}
 			</div>
-			<p className="mt-1 text-sm font-semibold text-gray-900">{formatPrice(price / 100)}</p>
+			<p className="mt-1 text-sm font-semibold text-gray-900" data-testid="product-price">
+				{formatPrice(price / 100)}
+			</p>
 		</div>
 	);
 };
