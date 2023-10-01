2ba72dbc65ff1e40d4c1fb535c70fd8c7abbdd40
s1avo <s.nadolny@hotmail.com>
2023-10-01 19:59:45 +0200

    feat(products): add sorting by price

--- src/api/getProductsList.ts
+++ src/api/getProductsList.ts
@@ -6,13 +6,22 @@ import {
 	ProductsCountDocument,
 	ProductsGetListSearchDocument,
 	ProductsCountByCategorySlugDocument,
+	type ProductOrderByInput,
 } from "@/gql/graphql";
 import { executeGraphql } from "@/api/graphqlApi";
 
-export const getProductsList = async (limit: number, offset: number) => {
-	const graphqlResponse = await executeGraphql(ProductGetListDocument, {
-		limit,
-		offset,
+export const getProductsList = async (
+	limit: number,
+	offset: number,
+	orderBy: ProductOrderByInput | undefined,
+) => {
+	const graphqlResponse = await executeGraphql({
+		query: ProductGetListDocument,
+		variables: {
+			limit,
+			offset,
+			orderBy,
+		},
 	});
 
 	return graphqlResponse.productsConnection;
@@ -23,48 +32,63 @@ export const getProductsListByCategorySlug = async (
 	limit: number,
 	offset: number,
 ) => {
-	const graphqlResponse = await executeGraphql(ProductsGetListByCategorySlagDocument, {
-		slag: category,
-		limit,
-		offset,
+	const graphqlResponse = await executeGraphql({
+		query: ProductsGetListByCategorySlagDocument,
+		variables: {
+			slag: category,
+			limit,
+			offset,
+		},
 	});
 
 	return graphqlResponse.productsConnection;
 };
 
 export const getProductsListByCollectionSlag = async (collection: string) => {
-	const graphqlResponse = await executeGraphql(ProductsGetListByCollectionSlagDocument, {
-		slag: collection,
+	const graphqlResponse = await executeGraphql({
+		query: ProductsGetListByCollectionSlagDocument,
+		variables: {
+			slag: collection,
+		},
 	});
 
 	return graphqlResponse.products;
 };
 
 export const getProductsSuggestedList = async (collection: string) => {
-	const graphqlResponse = await executeGraphql(ProductsGetSuggestedListDocument, {
-		name: collection,
+	const graphqlResponse = await executeGraphql({
+		query: ProductsGetSuggestedListDocument,
+		variables: {
+			name: collection,
+		},
 	});
 
 	return graphqlResponse.products;
 };
 
 export const getProductsCount = async () => {
-	const graphqlResponse = await executeGraphql(ProductsCountDocument, {});
+	const graphqlResponse = await executeGraphql({ query: ProductsCountDocument, variables: {} });
 
 	return graphqlResponse.productsConnection.aggregate.count;
 };
 
 export const getProductsCountInCategory = async (slug: string) => {
-	const graphqlResponse = await executeGraphql(ProductsCountByCategorySlugDocument, {
-		slug,
+	const graphqlResponse = await executeGraphql({
+		query: ProductsCountByCategorySlugDocument,
+		variables: {
+			slug,
+		},
 	});
 
 	return graphqlResponse.categories[0]?.products.length;
 };
 
 export const getSearchProductsList = async (search: string) => {
-	const graphqlResponse = await executeGraphql(ProductsGetListSearchDocument, {
-		search,
+	const graphqlResponse = await executeGraphql({
+		query: ProductsGetListSearchDocument,
+		variables: {
+			search,
+		},
 	});
 
 	return graphqlResponse.products;
