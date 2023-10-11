02f859928c3c454c7418f428dcfbd7738bbf151b
s1avo <s.nadolny@hotmail.com>
2023-10-11 20:43:37 +0200

    ref(): revert hygraph tests

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,26 @@
-// import { authMiddleware } from "@clerk/nextjs";
-//
-// export default authMiddleware({
-// 	publicRoutes: [
-// 		"/",
-// 		"/search",
-// 		"/cart",
-// 		"/categories/accessories",
-// 		"/categories/accessories(.*)",
-// 		"/categories/hoodies",
-// 		"/categories/hoodies(.*)",
-// 		"/categories/t-shirts",
-// 		"/categories/t-shirts(.*)",
-// 		"/collections",
-// 		"/collections(.*)",
-// 		"/products",
-// 		"/products(.*)",
-// 		"/product",
-// 		"/product(.*)",
-// 	],
-// });
-//
-// export const config = {
-// 	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// 	// matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+	publicRoutes: [
+		"/",
+		"/search",
+		"/cart",
+		"/categories/accessories",
+		"/categories/accessories(.*)",
+		"/categories/hoodies",
+		"/categories/hoodies(.*)",
+		"/categories/t-shirts",
+		"/categories/t-shirts(.*)",
+		"/collections",
+		"/collections(.*)",
+		"/products",
+		"/products(.*)",
+		"/product",
+		"/product(.*)",
+	],
+});
+
+export const config = {
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+	// matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
